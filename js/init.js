a2870662414b9562fe7355e6bd8e3ef286366fb1
Kaley Fry <kaleyfry@gmail.com>
2025-01-11 12:19:48 -0500

    Update main page and animateSpan

--- js/init.js
+++ js/init.js
@@ -139,7 +139,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "Software Developer", "Community Builder", "Systems Administrator", "Nonprofit Manager", "Leader"],
+			strings: ["Software Engineer", "Community Builder", "Freelancer", "Systems Administrator", "Nonprofit Manager", "Leader"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
